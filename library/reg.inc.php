487b7b2ce1a21af65f32e5b87c25f492ab36aea3
bob <616096669@qq.com>
2017-11-22 11:21:30 +0800

    add new project

--- library/reg.inc.php
+++ library/reg.inc.php
@@ -1,9 +1,9 @@
 <h2>请注册您的个人信息</h2>
 <div class="reg">
-	<form method="post" action="">
+	<form method="post" action="index.php">
 		<p>用户名: <input type="text" name="username" value=""></p>
 		<p>密   码: <input type="password" name="password" value=""></p>
-		<p>密码确认: <input type="password" name="password" value=""></p>
+		<p>密码确认: <input type="password" name="notpassword" value=""></p>
 		<p>电子邮件: <input type="text" name="email" value=""></p>
 		<p><input type="submit" name="send" value="注册"></p>
 		<p>[<a href="?">返回上一层</a>]</p>
